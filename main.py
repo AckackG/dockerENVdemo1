6f5058fd8268bbea6239e2c55a4414877c7f180f
ACKACKs <ackack222@outlook.com>
2024-08-12 16:09:10 +0800

    5

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from os import environ
-
+from time import sleep
 
 def main():
+    while True:
     # show all env k,v
-    print('-'*20)
-    for k, v in environ.items():
-        print(f'{k} = {v}\n')
+        print('-'*20)
+        for k, v in environ.items():
+            print(f'{k} = {v}\n')
 
-    print('ENV_FILE_FOO: ', environ.get('ENV_FILE_FOO'))
-    print('ENV_FILE_BAR: ', environ.get('ENV_FILE_BAR'))
-    print('DOT_ENV_VARIABLE: ', environ.get('DOT_ENV_VARIABLE'))
-    print('ENV_FROM_DOCKERFILE: ', environ.get('ENV_FROM_DOCKERFILE'))
-    print('ENV_FROM_DOCKERFILE_2: ', environ.get('ENV_FROM_DOCKERFILE_2'))
+        print('ENV_FILE_FOO: ', environ.get('ENV_FILE_FOO'))
+        print('ENV_FILE_BAR: ', environ.get('ENV_FILE_BAR'))
+        print('DOT_ENV_VARIABLE: ', environ.get('DOT_ENV_VARIABLE'))
+        print('ENV_FROM_DOCKERFILE: ', environ.get('ENV_FROM_DOCKERFILE'))
+        print('ENV_FROM_DOCKERFILE_2: ', environ.get('ENV_FROM_DOCKERFILE_2'))
 
+        sleep(10)
 
 if __name__ == '__main__':
     main()
