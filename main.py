dedace35f411e9efd0664bc3e83c666332cbe908
AckackG <110320811+AckackG@users.noreply.github.com>
2024-09-04 10:53:02 +0800

    update

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
         print('ENV_FILE_FOO: ', environ.get('ENV_FILE_FOO'))
         print('ENV_FILE_BAR: ', environ.get('ENV_FILE_BAR'))
         print('DOT_ENV_VARIABLE: ', environ.get('DOT_ENV_VARIABLE'))
+        print('DOT_ENV_VARIABLE2: ', environ.get('DOT_ENV_VARIABLE2'))
         print('ENV_FROM_DOCKERFILE: ', environ.get('ENV_FROM_DOCKERFILE'))
         print('ENV_FROM_DOCKERFILE_2: ', environ.get('ENV_FROM_DOCKERFILE_2'))
 
